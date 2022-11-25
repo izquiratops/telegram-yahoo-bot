2a7a6111d341e3bb7a542d8d626ebd67a5cea381
Jordi Izquierdo <hi@izquiratops.dev>
2022-11-25 09:11:14 +0100

    Before christmas flavor

---
+++ README.md
@@ -0,0 +1,1 @@
+Add a `.telegram-bot-token` on the path where you're going to run the docker compose.
--- src/modules/telegramBot.py
+++ src/modules/telegramBot.py
@@ -1,4 +1,3 @@
-import os
 from telegram.ext.updater import Updater
 
 
@@ -14,5 +13,6 @@ class TelegramBot:
         self.updater.idle()
 
     def __init__(self) -> None:
-        token = os.environ.get('TELEGRAM_TOKEN')
+        token_path = f'/home/.telegram-bot-token'
+        token = open(token_path).read().rstrip('\n')
         self.updater = Updater(token, use_context=True)
